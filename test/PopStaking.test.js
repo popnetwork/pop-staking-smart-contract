487874f0e0f91f8ac745f1f5845413838e12487c
Jack Li <jackz163299@gmail.com>
2021-04-19 05:28:33 -0400

    add mocks and unittest

---
+++ test/PopStaking.test.js
@@ -0,0 +1,61 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { time } = require('@openzeppelin/test-helpers');
+const Web3 = require('web3');
+const { utils } = Web3;
+
+const {
+  isCallTrace,
+} = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+
+const PROMO = 0;
+
+describe("PopStaking contract", function () {
+  before(async function () {
+    this.PopStaking = await ethers.getContractFactory("PopStaking");
+    [
+      this.owner, 
+      this.alice,
+      this.bob,
+      this.carol,
+      this.dev,
+      this.minter,
+      ...addrs
+    ] = await ethers.getSigners();
+    this.ERC20Mock = await ethers.getContractFactory("ERC20Mock", this.minter)
+  })
+
+  beforeEach(async function () {
+    this.pop = await this.ERC20Mock.deploy("Popnetwork", "POP", utils.toWei('10000000000000000'))
+    this.currentTime = (await time.latest()) * 1.0
+    this.pool = await this.PopStaking.deploy(this.pop.address, this.dev.address, this.currentTime, 1000000000)
+    this.startBlock = parseInt(await this.pool.startBlock())
+    this.startTime = parseInt(await this.pool.startTime())
+  });
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await this.pool.owner()).to.equal(this.owner.address)
+    });
+
+    it("Should set correct state variables", async function () {
+        expect(await this.pool.devaddr()).to.equal(this.dev.address)
+        expect(await this.startTime).to.equal(this.currentTime)
+        expect(await this.pool.popPerBlock()).to.equal(1000000000)
+    });
+
+  });
+
+  context("Staking", function () {
+    beforeEach(async function () {
+      await this.pop.transfer(this.alice.address, utils.toWei("1000000"))
+      expect(await this.pop.balanceOf(this.alice.address)).to.equal(utils.toWei("1000000"))
+      await this.pop.transfer(this.bob.address, utils.toWei("1000000"))
+      expect(await this.pop.balanceOf(this.bob.address)).to.equal(utils.toWei("1000000"))
+    })
+
+  })
+});
+
+
+
