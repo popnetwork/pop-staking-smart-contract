a84379b878dc2599eade10111d95a6e32c1f6dfc
Jack <jackz163299@gmail.com>
2022-03-14 15:50:08 -0400

    floating pragma

--- contracts/PopStaking.sol
+++ contracts/PopStaking.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.6.12;
+pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol";
